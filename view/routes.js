af29eaaa3fb1ca34cba5af1c57f77ea3b3e28dbe
MOHAMED LAAGUILI | Junior Dev <94993660+MOHAMED-LAAGUILI@users.noreply.github.com>
2024-12-10 10:40:16 +0100

    Update routes.js

--- view/routes.js
+++ view/routes.js
@@ -1,11 +1,18 @@
 import express from "express";
-import { addUserController, deleteUserController, getUsersDataController, updateUserController } from "../controller/user.controller.js";
+import {
+  addUserController,
+  deleteUserController,
+  getUserByIdController,
+  getUsersDataController,
+  updateUserController,
+} from "../controller/user.controller.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/get-all-users-data', getUsersDataController )
-router.post('/add-user', addUserController )
-router.put('/update-user/:userid', updateUserController )
-router.delete('/delete-user/:userid', deleteUserController )
+router.get("/get-all-users-data", getUsersDataController);
+router.get("/get-one-user-data/:userid", getUserByIdController);
+router.post("/add-user", addUserController);
+router.put("/update-user/:userid", updateUserController);
+router.delete("/delete-user/:userid", deleteUserController);
 
 export default router;
